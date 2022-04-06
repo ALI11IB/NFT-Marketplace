806092ca653fae599bfa4bb48a3afb08d27f527e
ALI11IB <ali.la.ibrahem@gmail.com>
2022-04-06 13:17:54 -0700

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,13 +23,29 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  defaultNetwork: "hardhat",
   networks: {
+    hardhat: {
+      chainId: 1337,
+    },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    // mumbai: {
+    //   url: "https://rpc-mumbai.maticvigil.com",
+    //   accounts: [process.env.privateKey],
+    // },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
